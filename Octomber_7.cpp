05911624f82d9af8a14d312870882e71d46c9779
samahakal <91861509+samahakal@users.noreply.github.com>
2022-10-07 20:52:50 +0530

    732. My Calendar III

---
+++ Octomber_7.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/my-calendar-iii/description/
+// 732. My Calendar III
+
+class MyCalendarThree {
+private:
+    int mxCnt = 0;
+    map<int,int>mp;
+public:
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int start, int end) {
+        mp[start]++;
+        mp[end]--;
+
+        int overlap = 0;
+        for(auto it : mp){
+            overlap += it.second;
+            mxCnt = max(mxCnt,overlap);
+        }
+        return mxCnt;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
