be523baef1d69f9351adcd307ba4fc07888efe06
samahakal <91861509+samahakal@users.noreply.github.com>
2022-10-31 10:57:15 +0530

    766. Toeplitz Matrix

--- Octomber_31.cpp
+++ Octomber_31.cpp
@@ -1,6 +1,7 @@
 // 766. Toeplitz Matrix
 // https://leetcode.com/problems/toeplitz-matrix/description/
 
+Method 1 
 class Solution {
 public:
     int row,col;
@@ -26,6 +27,27 @@ public:
         for(int i=1;i<row;i++){
             if(check(matrix,i,0) == false){
                 return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+Method 2 
+class Solution {
+public:
+
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]) return false;
             }
         }
         return true;
